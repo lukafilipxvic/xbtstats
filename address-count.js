6653c561d71e8b7d495879388ecf4f6b28de7ba8
Doncic <traderdoncic@protonmail.com>
2023-04-02 21:33:25 +1000

    Wallet coin selection
    Added ETH & DOGE selection to address count. Auto updates plotly charts.

--- address-count.js
+++ address-count.js
@@ -1,6 +1,195 @@
-d3.csv(
-    "https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets=btc&metrics=PriceUSD,AdrBalNtv1Cnt,AdrBalNtv10Cnt,AdrBalNtv100Cnt,AdrBalNtv1KCnt,AdrBalNtv10KCnt,AdrBalNtv100KCnt&page_size=10000&pretty=true&format=csv",
-    function (err, rows) {
+document.addEventListener('DOMContentLoaded', function () {
+
+    const coinSelect = document.getElementById('coin');
+    var selectedCoin = coinSelect.value; // default selection
+
+    coinSelect.addEventListener('change', () => {
+        var selectedCoin = coinSelect.value;
+
+        d3.csv(`https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets=${selectedCoin}&metrics=PriceUSD,AdrBalNtv1Cnt,AdrBalNtv10Cnt,AdrBalNtv100Cnt,AdrBalNtv1KCnt,AdrBalNtv10KCnt,AdrBalNtv100KCnt&page_size=10000&pretty=true&format=csv`, function (err, rows) {
+            function unpack(rows, key) {
+                return rows.map(function (row) {
+                    return row[key];
+                });
+            }
+
+            var min_1_count = {
+                type: "scatter",
+                mode: "lines",
+                name: "Addresses with Balance > 1",
+                x: unpack(rows, "time"),
+                y: unpack(rows, "AdrBalNtv1Cnt"),
+                line: {
+                    color: "#f2a900",
+                    width: 1.5,
+                }
+            };
+
+            var min_10_count = {
+                type: "scatter",
+                mode: "lines",
+                name: "Addresses with Balance > 10",
+                x: unpack(rows, "time"),
+                y: unpack(rows, "AdrBalNtv10Cnt"),
+                line: {
+                    color: "#f2a900",
+                    width: 1.5,
+                }
+            };
+
+            var min_100_count = {
+                type: "scatter",
+                mode: "lines",
+                name: "Addresses with Balance > 100",
+                x: unpack(rows, "time"),
+                y: unpack(rows, "AdrBalNtv100Cnt"),
+                line: {
+                    color: "#f2a900",
+                    width: 1.5,
+                }
+            };
+
+            var min_1k_count = {
+                type: "scatter",
+                mode: "lines",
+                name: "Addresses with Balance > 1k",
+                x: unpack(rows, "time"),
+                y: unpack(rows, "AdrBalNtv1KCnt"),
+                line: {
+                    color: "#f2a900",
+                    width: 1.5,
+                }
+            };
+
+            var min_10k_count = {
+                type: "scatter",
+                mode: "lines",
+                name: "Addresses with Balance > 10k",
+                x: unpack(rows, "time"),
+                y: unpack(rows, "AdrBalNtv10KCnt"),
+                line: {
+                    color: "#f2a900",
+                    width: 1.5,
+                }
+            };
+            var min_100k_count = {
+                type: "scatter",
+                mode: "lines",
+                name: "Addresses with Balance > 100k",
+                x: unpack(rows, "time"),
+                y: unpack(rows, "AdrBalNtv100KCnt"),
+                line: {
+                    color: "#f2a900",
+                    width: 1.5,
+                }
+            };
+
+            var price = {
+                type: "scatter",
+                mode: "lines",
+                name: "Price (USD)",
+                yaxis: 'y2',
+                x: unpack(rows, "time"),
+                y: unpack(rows, "PriceUSD"),
+                line: {
+                    color: "#121212",
+                    width: 1.5,
+                }
+            };
+
+            var data = [min_1_count, price];
+            var data2 = [min_10_count, price];
+            var data3 = [min_100_count, price];
+            var data4 = [min_1k_count, price];
+            var data5 = [min_10k_count, price];
+            var data6 = [min_100k_count, price];
+
+
+
+            var layout = {
+                autosize: true,
+                height: 400,
+                showlegend: true,
+                legend: {
+                    orientation: "h",
+                    x: 0.5,
+                    xanchor: "center",
+                },
+
+                images: [{
+                    source: "/xbtstats.png",
+                    x: 0.5,
+                    y: 0.5,
+                    sizex: 0.4,
+                    sizey: 0.4,
+                    xanchor: "center",
+                    xref: "paper",
+                    yanchor: "bottom",
+                    yref: "paper",
+                    layer: "below",
+                    opacity: 0.1,
+                }],
+                margin: {
+                    t: 0,
+                },
+                font: {
+                    color: '#121212',
+                    size: 11,
+                },
+                xaxis: {
+                    gridthickness: 2,
+                    showspikes: true,
+                    spikemode: 'toaxis',
+                    spikethickness: 0.5,
+                    spikecolor: '#333',
+                    spikedash: 'line',
+                    showline: true,
+                },
+
+                yaxis: {
+                    title: "Number of Balances",
+                    type: "linear",
+                    rangemode: 'nonnegative',
+                    autorange: true,
+                    showgrid: false,
+                    showspikes: true,
+                    spikemode: 'across',
+                    spikethickness: 0.5,
+                    spikecolor: '#333',
+                    spikedash: 'line',
+                    showline: true,
+                },
+
+                yaxis2: {
+                    title: 'Price (USD)',
+                    type: 'log',
+                    rangemode: 'nonnegative',
+                    autorange: true,
+                    showgrid: false,
+                    tickformat: '$,.0f',
+                    hoverformat: '$,.2f',
+                    dtick: 1,
+                    showspikes: true,
+                    spikemode: 'across',
+                    spikethickness: 0.5,
+                    spikecolor: '#333',
+                    spikedash: 'line',
+                    overlaying: 'y',
+                    side: 'right'
+                },
+            };
+
+            Plotly.newPlot("min_1_count-chart", data, layout, { responsive: true, displaylogo: false });
+            Plotly.newPlot("min_10_count-chart", data2, layout, { responsive: true, displaylogo: false });
+            Plotly.newPlot("min_100_count-chart", data3, layout, { responsive: true, displaylogo: false });
+            Plotly.newPlot("min_1k_count-chart", data4, layout, { responsive: true, displaylogo: false });
+            Plotly.newPlot("min_10k_count-chart", data5, layout, { responsive: true, displaylogo: false });
+            Plotly.newPlot("min_100k_count-chart", data6, layout, { responsive: true, displaylogo: false });
+        });
+    });
+
+
+    d3.csv(`https://community-api.coinmetrics.io/v4/timeseries/asset-metrics?assets=${selectedCoin}&metrics=PriceUSD,AdrBalNtv1Cnt,AdrBalNtv10Cnt,AdrBalNtv100Cnt,AdrBalNtv1KCnt,AdrBalNtv10KCnt,AdrBalNtv100KCnt&page_size=10000&pretty=true&format=csv`, function (err, rows) {
         function unpack(rows, key) {
             return rows.map(function (row) {
                 return row[key];
@@ -10,7 +199,7 @@ d3.csv(
         var min_1_count = {
             type: "scatter",
             mode: "lines",
-            name: "Addresses with Balance > 1 BTC",
+            name: "Addresses with Balance > 1",
             x: unpack(rows, "time"),
             y: unpack(rows, "AdrBalNtv1Cnt"),
             line: {
@@ -22,7 +211,7 @@ d3.csv(
         var min_10_count = {
             type: "scatter",
             mode: "lines",
-            name: "Addresses with Balance > 10 BTC",
+            name: "Addresses with Balance > 10",
             x: unpack(rows, "time"),
             y: unpack(rows, "AdrBalNtv10Cnt"),
             line: {
@@ -34,7 +223,7 @@ d3.csv(
         var min_100_count = {
             type: "scatter",
             mode: "lines",
-            name: "Addresses with Balance > 100 BTC",
+            name: "Addresses with Balance > 100",
             x: unpack(rows, "time"),
             y: unpack(rows, "AdrBalNtv100Cnt"),
             line: {
@@ -46,7 +235,7 @@ d3.csv(
         var min_1k_count = {
             type: "scatter",
             mode: "lines",
-            name: "Addresses with Balance > 1k BTC",
+            name: "Addresses with Balance > 1k",
             x: unpack(rows, "time"),
             y: unpack(rows, "AdrBalNtv1KCnt"),
             line: {
@@ -58,7 +247,7 @@ d3.csv(
         var min_10k_count = {
             type: "scatter",
             mode: "lines",
-            name: "Addresses with Balance > 10k BTC",
+            name: "Addresses with Balance > 10k",
             x: unpack(rows, "time"),
             y: unpack(rows, "AdrBalNtv10KCnt"),
             line: {
@@ -69,7 +258,7 @@ d3.csv(
         var min_100k_count = {
             type: "scatter",
             mode: "lines",
-            name: "Addresses with Balance > 100k BTC",
+            name: "Addresses with Balance > 100k",
             x: unpack(rows, "time"),
             y: unpack(rows, "AdrBalNtv100KCnt"),
             line: {
@@ -81,7 +270,7 @@ d3.csv(
         var price = {
             type: "scatter",
             mode: "lines",
-            name: "BTC Price (USD)",
+            name: "Price (USD)",
             yaxis: 'y2',
             x: unpack(rows, "time"),
             y: unpack(rows, "PriceUSD"),
@@ -155,7 +344,7 @@ d3.csv(
             },
 
             yaxis2: {
-                title: 'BTC Price (USD)',
+                title: 'Price (USD)',
                 type: 'log',
                 rangemode: 'nonnegative',
                 autorange: true,
@@ -170,7 +359,7 @@ d3.csv(
                 spikedash: 'line',
                 overlaying: 'y',
                 side: 'right'
-            }
+            },
         };
 
         Plotly.newPlot("min_1_count-chart", data, layout, { responsive: true, displaylogo: false });
@@ -180,3 +369,4 @@ d3.csv(
         Plotly.newPlot("min_10k_count-chart", data5, layout, { responsive: true, displaylogo: false });
         Plotly.newPlot("min_100k_count-chart", data6, layout, { responsive: true, displaylogo: false });
     });
+});
--- messari.js
+++ messari.js
@@ -15,5 +15,5 @@ window
     .then(
         payload =>
         (document.getElementById("btcspot").innerHTML =
-            "Current spot price: $" + payload.data.market_data.price_usd.toLocaleString())
+            "Current BTC spot price: $" + payload.data.market_data.price_usd.toLocaleString())
     );
